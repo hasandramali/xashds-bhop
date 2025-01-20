c799f124cc6335775720e661e6f114f1e5836d27
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-01-20 23:23:56 +0200

    Update startcsv.sh

--- startcsv.sh
+++ startcsv.sh
@@ -1,1 +1,1 @@
-screen ./xash3d.sh -game cstrike +public 1 +port 27015 +deathmatch 1 +maxplayers 30 +map bhop_m_skill4 +hostname "Bhop Server"
+./xash3d.sh -game cstrike +public 1 +sv_nat 1 +deathmatch 1 +maxplayers 30 +map bhop_m_skill4 +hostname "Bhop Server"
